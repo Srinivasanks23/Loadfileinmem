7fdafe6995460437716e5e2ab0ed8de562adf28c
srinivasanks23 <srinivasanks23@gmail.com>
2015-11-05 10:41:42 -0500

    Thread execution

---
+++ ThreadExecutionTest/ThreadExecutionTest/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExecutionTest
+{
+    class Program
+    {
+        private static SemaphoreSlim semaphore;
+
+        // A padding interval to make the output more orderly.
+        private static int padding;
+
+        static void Main(string[] args)
+        {
+            semaphore = new SemaphoreSlim(10, 10);
+
+            Console.WriteLine("{0} tasks can enter the semaphore.",
+                          semaphore.CurrentCount);
+            Task[] tasks = new Task[10];
+
+            try
+            {
+                // Create and start five numbered tasks.
+                for (int i = 0; i < 10; i++)
+                {
+                    tasks[i] = Task.Run(() => Process());
+                }
+
+                // Wait for half a second, to allow all the tasks to start and block.
+                Thread.Sleep(500);
+
+                // Restore the semaphore count to its maximum value.
+                Console.Write("Main thread calls Release(3) --> ");
+                Console.WriteLine("{0} tasks can enter the semaphore.",
+                                  semaphore.CurrentCount);
+                // Main thread waits for the tasks to complete.
+
+
+                while (semaphore.CurrentCount != 10)
+                {
+                    Thread.Sleep(50);
+                    Console.WriteLine("Current count is " + semaphore.CurrentCount);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Main thread exits.");
+            Console.ReadKey();
+        }
+
+        private static void Process()
+        {
+            try
+            {
+                // Each task begins by requesting the semaphore.
+                Console.WriteLine("Task {0} begins and waits for the semaphore.",
+                                  Task.CurrentId);
+
+                semaphore.Wait();
+
+                Interlocked.Add(ref padding, 100);
+
+                Console.WriteLine("Task {0} enters the semaphore.", Task.CurrentId);
+
+                // The task just sleeps for 1+ seconds.
+                Thread.Sleep(4000 + padding);
+
+                //Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.",
+                  //                Task.CurrentId, semaphore.Release());
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
---
+++ ThreadExecutionTest/ThreadExecutionTest/ThreadExecutionSample.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExecutionTest
+{
+    class ThreadExecutionSample
+    {
+
+        public void Process()
+        {
+            int maxElements = 500000;
+            int maxArrays = 500;
+            int[][] numbers = new int[maxArrays][];
+            bool[] booleans = new bool[maxArrays];
+            List<int> maxLog = new List<int>();
+            List<int> maxLog2 = new List<int>();
+
+            AutoResetEvent event1 = new AutoResetEvent(false);
+            AutoResetEvent event2 = new AutoResetEvent(false);
+
+            Random rand = new Random(1000000);
+
+            //Feed the value to the number array
+            for (int i = 0; i < maxArrays; i++)
+            {
+                numbers[i] = new int[maxElements];
+
+                for (int j = 0; j < maxElements; j++)
+                {
+                    numbers[i][j] = rand.Next();
+                }
+            }
+
+            List<Task> taskList = new List<Task>();
+            //StreamWriter writer = File.AppendText(@"C:\Users\kesavsr\Documents\Log.txt");
+            Console.WriteLine("Done building data" + DateTime.Now);
+            try
+            {
+                for (int i = 0; i < maxArrays; i++)
+                {
+                    taskList.Add(Task.Factory.StartNew((x) =>
+                    {
+                        //Console.WriteLine("Running the task");
+                        //Interlocked.Increment(ref threadCount);
+                        booleans[(int)x] = true;
+                        event1.Set();
+                        Thread.Sleep(1);
+                        event2.WaitOne();
+                        booleans[(int)x] = false;
+
+                    }, i));
+                }
+
+                //Task.WaitAll(taskList.ToArray());
+
+                /*while (!Task.WhenAll(taskList.ToArray()).IsCompleted)
+                {
+                    maxLog.Add(booleans.Count((x) => x == true));
+                    //Thread.Sleep(50);
+                }*/
+
+                while (!Task.WhenAll(taskList.ToArray()).IsCompleted)
+                {
+                    event1.WaitOne(500);
+                    maxLog.Add(booleans.Count((x) => x == true));
+
+                    /*
+                    int count = 0;
+
+                    foreach(ProcessThread t in Process.GetCurrentProcess().Threads)
+                    {
+                         ++count;
+                    }
+                    maxLog2.Add(count);*/
+
+                    event2.Set();
+                    //Thread.Sleep(50);
+                }
+
+                // writer.WriteLine("Logging ends here " + DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            finally
+            {
+                /* if (writer != null)
+                {
+                    writer.Close();
+                } */
+            }
+
+            Console.WriteLine("max ThreadCount : " + maxLog.Max());
+            //Console.WriteLine("max ThreadCount2 : " + maxLog2.Max());
+            Console.ReadLine();
+        }
+    }
+}
